33bcee622942b4d5d8a4e22c23bd4b809f16cd9a
Andrew <ChipShop@users.noreply.github.com>
2016-03-24 13:24:54 +0000

    Added support for negative numbers and reading from stdin

--- Intermediate Challenges/Challenge #0008 [Intermediate]/solutions/solution.c
+++ Intermediate Challenges/Challenge #0008 [Intermediate]/solutions/solution.c
@@ -1,6 +1,10 @@
+#include <ctype.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+
+#define BUFF_SIZE 1024
 
 static char *invoc_name = NULL;
 static char *version_string = "1.0";
@@ -31,6 +35,7 @@ static void usage(void);
 static void version(void);
 
 static void print_number(int number);
+static char *read_line(void);
 
 int main(int argc, char **argv){
     int c;
@@ -62,69 +67,129 @@ int main(int argc, char **argv){
         }
     }
     else {
-        ; /* print it for each line of input */
+        int number;
+        char *line = NULL; /* will be dynamically allocated by read_line */
+        while ( (line = read_line()) != NULL ) {
+            if ( sscanf(line, "%d", &number) != 1 ) {
+                fprintf(stderr, "Invalid number: %s\n", line);
+            }
+            else {
+                print_number(number);
+                puts("");
+            }
+
+            free(line); 
+        }
     }
 
     return 0;
 }
 
 static void print_number(int number){
-    if(number < 20){
+    if ( number == INT_MIN ) {
+        /* Multiplying it by -1 will result in overflow */
+        fprintf(stderr, "Number is too small: %d.\n", number);
+        return;
+    }
+    if ( number < 0 ) {
+        printf("minus ");
+        number *= -1;
+    }
+
+    if (number < 20) {
         printf("%s",numbers[number]);
     }
-    else if(number < HUNDRED){
+    else if (number < HUNDRED) {
         printf("%s",tens[number / 10 - 2]);
         number %= 10;
-        if(number > 0){
+        if (number > 0) {
             printf("-");
             print_number(number);
         }
     }
-    else if(number < THOUSAND){
+    else if (number < THOUSAND) {
         printf("%s",numbers[number / HUNDRED]);
         printf("%s"," hundred");
         number %= HUNDRED;
-        if(number > 0){
+        if (number > 0) {
             printf(" and ");
             print_number(number);
         }
     }
-    else if(number < MILLION){
+    else if (number < MILLION) {
         print_number(number / 1000);
         printf("%s"," thousand");
         number %= 1000;
-        if(number > 0){
-            if(number < HUNDRED)
+        if (number > 0) {
+            if (number < HUNDRED)
                 printf(" and ");
             else
                 printf(", ");
             print_number(number);
         }
     }
-    else if(number < MILLIARD){
+    else if (number < MILLIARD) {
         print_number(number / MILLION);
         printf("%s"," million");
         number %= MILLION;
-        if(number > 0){
-            if(number < HUNDRED)
+        if (number > 0) {
+            if (number < HUNDRED)
                 printf(" and ");
             else
                 printf(", ");
             print_number(number);
         }
     }
-    else if(number < BILLION){
+    else if (number < BILLION) {
         print_number(number / MILLIARD);
         printf("%s"," milliard");
         number %= MILLIARD;
-        if(number > 0){
-            if(number < HUNDRED)
+        if (number > 0) {
+            if (number < HUNDRED)
                 printf(" and ");
             else
                 printf(", ");
             print_number(number);
         }
     }
+}
+
+/* need to free buffer allocated by this after use */
+static char *read_line(void){
+    char *line = malloc(sizeof(char) * (BUFF_SIZE + 1));
+    int c;
+    int i = 0;
+    int length = BUFF_SIZE;
+
+    if ( line == NULL ) {
+        perror("Cannot allocate buffer");
+        exit(EXIT_FAILURE);
+    }
+    
+    while ( ( c = getchar() ) != EOF && c != '\n' ) {
+        if ( i >= length ) {
+            char *tmp = realloc(line, length + BUFF_SIZE);
+
+            if (tmp == NULL) {
+                free(line);
+                perror("Cannot reallocate buffer");
+                exit(EXIT_FAILURE);
+            }
+
+            length += BUFF_SIZE;
+        }
+        line[i++] = c;
+    }
+
+    /* if we reached EOF */
+    if ( i == 0 ) {
+        free(line);
+        return NULL;
+    }
+
+    line[i] = '\0';
+
+    return line;
 }
 
 static void usage(void){
