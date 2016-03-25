540082c12c1639583b211aef4c03989edf91ed46
Andrew <ChipShop@users.noreply.github.com>
2016-03-25 12:46:31 +0000

    Finished Perl solution for 'Hard' #0001 --- number guesser

--- Hard Challenges/Challenge #0001 [Hard]/solutions/solution.pl
+++ Hard Challenges/Challenge #0001 [Hard]/solutions/solution.pl
@@ -1,31 +1,43 @@
 #!/usr/bin/perl --
 
-use v5.18;
-#use warnings;
+use v5.22;
+use warnings;
+use integer;
 
-my @range = 1..100; # viable numbers to guess
-my $high = $#range;
-my $low = 0;
+my $high = 100;
+my $low = 1;
 
-say "Please think of a number between 1 and 100 inclusive.";
+say "Please think of a number between $low and $high inclusive.";
 
-while($high > $low) {
-    my $guess = $range[ ($high + $low) / 2 ];
-    say "I guess $guess, is that correct, too high, or too low?";
+GUESS:
+while($high >= $low) {
+    my $guess = ($high + $low) / 2;
+    say "I guess $guess, is that correct, too high or too low?";
 
-    my $response = <>;
-    chomp $response;
+    ANSWER:
+    while (1) {
+        my $response = <> // "";
+        chomp $response;
 
-    if ( $response eq "correct" ) {
-        say "Yay!";
-        last;
-    }
-    elsif ( $response eq "high" ) {
-        say "Bah!";
-        $high = ( $high + $low ) / 2;
-    }
-    elsif ( $response eq "low" ) {
-        say "Bah!";
-        $low = ( $high + $low ) / 2;
+        if ( $response eq "correct" ) {
+            say "Yay!";
+            last GUESS;
+        }
+        elsif ( $response eq "high" ) {
+            say "Bah!";
+            $high = $guess - 1;
+            next GUESS;
+        }
+        elsif ( $response eq "low" ) {
+            say "Bah!";
+            $low = $guess + 1;
+            next GUESS;
+        }
+
+        say "My guess is $guess, please say 'correct' if I'm right, "
+           ."'high' if I've guessed too high or 'low' if I've guessed too low";
+        next ANSWER;
     }
 }
+
+say "You cheated!" if $high < $low;
