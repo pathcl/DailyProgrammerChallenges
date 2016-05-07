1d03cf28a68eda74f1e814195b3e68244da9062f
Freddie Vargus <fv4.programmer@gmail.com>
2016-05-07 08:21:52 -0400

    Add updated files to SMS

--- send-data.sh
+++ send-data.sh
@@ -4,8 +4,11 @@
 # Purpose: Automatically pushes changes to repo when post-challenges.py is run
 # Sends text to owner to notify about changes
 
+
+CHANGES="$(git diff --name-only)"
+
 git pull; git add -A; git commit -m "Add this weeks challenges"; git push
 
 read -r line < ../mynumber.txt
 
-echo "Repo Successfully Updated" | .././twilio-sms.sh -c /.twiliorc/config $line
+echo "Repo Successfully Updated with:" $CHANGES | .././twilio-sms.sh -c /.twiliorc/config $line
