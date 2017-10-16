5457f87e487794dc52a449e0f969a5fb7faff8fa
Altlock <altlockg@gmail.com>
2017-10-16 21:13:04 +0200

    Added file-writing bonus

--- Easy Challenges/Challenge 0001 Easy/solutions/solution.tcl
+++ Easy Challenges/Challenge 0001 Easy/solutions/solution.tcl
@@ -4,4 +4,11 @@ puts "What's your age in years? "
 set age [gets stdin]
 puts "What's your username? "
 set username [gets stdin]
-puts "Your name is $name, you are $age years old and your username is $username."
+set string "Your name is $name, you are $age years old and your username is $username."
+puts $string
+
+#Now put it in a file
+set file [open "result.txt" "w"]
+puts $file $string
+close $file
+
