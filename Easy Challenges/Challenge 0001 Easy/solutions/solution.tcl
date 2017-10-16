0461dbc2be70490fce02838fcdeec29548d4a0a1
Altlock <altlockg@gmail.com>
2017-10-16 21:04:42 +0200

    Warming up, first time committing code to public project

---
+++ Easy Challenges/Challenge 0001 Easy/solutions/solution.tcl
@@ -0,0 +1,7 @@
+puts "What's your name? "
+set name [gets stdin]
+puts "What's your age in years? "
+set age [gets stdin]
+puts "What's your username? "
+set username [gets stdin]
+puts "Your name is $name, you are $age years old and your username is $username."
